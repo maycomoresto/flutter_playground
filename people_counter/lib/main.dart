cb89504cccbfe9a28e73e8f64e790e8759c67d5d
dsamuel32 <diego.samuel.alves@gmail.com>
2019-10-11 22:08:35 -0300

    People Counter

---
+++ people_counter/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+    runApp(MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "People Counter",
+        home: Home()
+    ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+    int _people = 0;
+    String _infoText = "Pode Entrar!";
+
+    void _changePeople(int valor) {
+        setState(() {
+          _people += valor;
+          if (_people < 0) _infoText = "Mundo invertido!";
+          else if (_people <= 10) _infoText = "Pode Entrar!";
+          else _infoText = "Lotado!";
+        });
+    }
+
+
+    @override
+    Widget build(BuildContext context) {
+    return Stack(
+        children: <Widget>[
+            Image.asset(
+                "images/restaurant.jpg",
+                fit: BoxFit.cover,
+                height: 1000.0,
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                    Text(
+                        "Pode Entrar $_people",
+                        style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40.0
+                        ),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.all(10.0),
+                                child: FlatButton(
+                                    child: Text(
+                                        "+1",
+                                        style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 40.0
+                                        ),
+                                    ),
+                                    onPressed: () { _changePeople(1); },
+                                ),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.all(10.0),
+                                child: FlatButton(
+                                    child: Text(
+                                        "-1",
+                                        style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 40.0
+                                        ),
+                                    ),
+                                    onPressed: () { _changePeople(-1); },
+                                ),
+                            )
+                        ],
+                    ),
+                    Text(
+                        _infoText,
+                        style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30.0,
+                                fontStyle: FontStyle.italic
+                        ),
+                    )
+                ],
+            )
+        ],
+    );
+    }
+}
